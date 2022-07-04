22328372709f1ae3f71f402bee7a34eb082af813
shundahoy <shundahoy@gmail.com>
2022-07-04 17:15:50 +0900

    コミットテスト

--- src/components/SideListItem/SideListItem.js
+++ src/components/SideListItem/SideListItem.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Style from './SideListItem.module.scss'
-const SideListItem = ({id, src, title}) => {
+import React from "react";
+import { Link } from "react-router-dom";
+import Style from "./SideListItem.module.scss";
+const SideListItem = ({ id, src, title }) => {
   return (
-    <Link className={Style.item} to={{pathname: 'watch', search: `?v=${id}`}}>
+    <Link className={Style.item} to={{ pathname: "watch", search: `?v=${id}` }}>
       <img src={src} alt={title} />
       <div className={Style.info}>
         <span>{title}</span>
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default SideListItem
+export default SideListItem;
